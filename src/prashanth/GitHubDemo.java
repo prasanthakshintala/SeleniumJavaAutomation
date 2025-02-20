fe73f1b60edd3244833910490ca4922a9bf83414
prasanthakshintala <prasanthakshintala96@gmail.com>
2025-02-20 12:06:09 +0530

    Lost your password tcs added under prashanth package

--- src/prashanth/GitHubDemo.java
+++ src/prashanth/GitHubDemo.java
@@ -14,6 +14,14 @@ public class GitHubDemo {
 	  driver.findElement(By.xpath("//*[@name=\"log\"]")).sendKeys("prashanth");
 	  driver.findElement(By.xpath("//*[@name=\"pwd\"]")).sendKeys("Hello@123");
 	  driver.findElement(By.xpath("//*[@name=\"rememberme\"]")).click();
+	  driver.findElement(By.id("wp-submit")).click();
+  }
+  
+  @Test
+  public void techlearnlostyourpassword() {
+	  driver.get("https://www.techlearn.in/admin");	
+	  driver.findElement(By.className("wp-login-lost-password")).click();
+	  driver.findElement(By.xpath("//*[@name=\"user_login\"]")).sendKeys("prasanthakshintala96@gmail.com");
   }
   @BeforeTest
   public void beforeTest() {
